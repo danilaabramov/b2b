65fbfa40ffb9b4f4bcc7e276a664a4e8662bfc41
Danila Abramov <67269761+danilaabramov@users.noreply.github.com>
2023-06-03 00:44:42 +0400

    Add files via upload

---
+++ src/pages/B2BPurchasesPage.js
@@ -0,0 +1,7 @@
+export default function B2BPurchasesPage() {
+    return (
+        <div style={{width: '100%'}}>
+            <h1 className="h1-title" style={{textAlign: 'center'}}>Ведутся технические работы</h1>
+            <img style={{width: '100%'}} src={require("../images/tech.png")} alt="tech"></img>
+        </div>)
+}
